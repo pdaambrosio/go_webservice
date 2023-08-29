0e53eea1ec8cb6dd6288d4cc2472e6db476e0617
pdaambrosio <pda.ambrosio@gmail.com>
2023-08-28 23:11:19 -0300

    feat: print error

--- main.go
+++ main.go
@@ -9,9 +9,13 @@ import (
 )
 
 func main() {
+	// Register the user controller with the http package so that it can handle requests
 	controllers.RegisterControllers()
 
+	// Create a server that listens on port 3000 and passes all requests to the http package
 	err := http.ListenAndServe(":3000", nil)
+	// If there is an error, print it to the console and exit the program
+	// It's possible to use the log package to log the error to a file instead of printing it to the console, example: log.Fatal(http.ListenAndServe(":3000", nil))
 	if err != nil {
 		fmt.Println(err.Error())
 	}
