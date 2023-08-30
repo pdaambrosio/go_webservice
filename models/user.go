be1679fa123545fb31018e69c1395318d41a0508
pdaambrosio <pda.ambrosio@gmail.com>
2023-08-29 23:10:14 -0300

    feat: new functions GetUserByID, UpdateUser and RemoveUserByID

--- models/user.go
+++ models/user.go
@@ -1,13 +1,18 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 type User struct {
-	ID			int
-	FirstName	string
-	LastName	string
+	ID        int
+	FirstName string
+	LastName  string
 }
 
 var (
-	users []*User // slice of pointers to User
+	users  []*User // slice of pointers to User
 	nextID = 1
 )
 
@@ -16,8 +21,41 @@ func GetUsers() []*User {
 }
 
 func AddUser(u User) (User, error) {
+	if u.ID != 0 {
+		return User{}, errors.New("new user must not include ID or it must be set to zero")
+	}
+
 	u.ID = nextID
 	nextID++
 	users = append(users, &u)
 	return u, nil
+}
+
+func GetUserByID(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil // dereference pointer to User
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", id)
+}
+
+func UpdateUser(u User) (User, error) {
+	for i, candidate := range users {
+		if candidate.ID == u.ID {
+			users[i] = &u // update slice element
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", u.ID)
+}
+
+func RemoveUserByID(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:]...) // remove slice element
+			return nil
+		}
+	}
+	return fmt.Errorf("User with ID '%v' not found", id)
 }
